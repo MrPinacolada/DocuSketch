76a71a9f117cae52400a3cd163f7fdf74b28df6c
BOB <vasiasko112@gmail.com>
2023-06-13 20:15:40 +0400

    endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,8 @@ export class AppComponent {
     this.availableIcons = Object.values({ ...fas, ...far, ...fab });
   }
   showIcons = () => {
+    const randomIndex = Math.floor(Math.random() * this.availableIcons.length);
     setTimeout(() => {
-      const randomIndex = Math.floor(
-        Math.random() * this.availableIcons.length
-      );
       this.currentIcon = this.availableIcons[randomIndex];
       console.log('done!');
     }, 3000);
