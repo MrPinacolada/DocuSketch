f42e429c3c08d0a93518eab03c28952f456e6118
BOB <vasiasko112@gmail.com>
2023-06-13 20:10:19 +0400

    endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas, faBell, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  currentIcon: IconDefinition = faBell;
+  availableIcons: IconDefinition[] = [];
+  constructor(private library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+    this.availableIcons = Object.values({ ...fas, ...far, ...fab });
+  }
+  showIcons = () => {
+    const randomIndex = Math.floor(Math.random() * this.availableIcons.length);
+    this.currentIcon = this.availableIcons[randomIndex];
+  };
 }
